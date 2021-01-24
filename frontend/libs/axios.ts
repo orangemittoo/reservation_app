8ac7d686d4bf9c72cff0d9991afdf3de0de67e55
ikumi.toshihiko <ikumit.org@gmail.com>
2021-01-24 13:53:05 +0900

    added: axios

---
+++ frontend/libs/axios.ts
@@ -0,0 +1,133 @@
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import humps from 'humps';
+
+axios.interceptors.request.use(
+  function (config) {
+    if (process.browser && config.data && config.data instanceof FormData) {
+      return config;
+    }
+    config.params = config.params && humps.decamelizeKeys(config.params);
+    config.data = config.data && humps.decamelizeKeys(config.data);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    response.data = response.data && humps.camelizeKeys(response.data);
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
+const requestDefaultConfig = {
+  baseURL: '/api'
+};
+
+
+const loader = (() => {
+  let loadCounter = 0;
+  return {
+    status() {
+      return loadCounter;
+    },
+    increment() {
+      loadCounter = loadCounter + 1;
+      return loadCounter;
+    },
+    decrement() {
+      loadCounter = Math.max(loadCounter - 1, 0);
+      return loadCounter;
+    },
+    completed() {
+      return loadCounter === 0;
+    },
+    clear() {
+      loadCounter = 0;
+    }
+  };
+})();
+
+export const errorHandler = (hideErrorMessage = false) => (
+  err: AxiosError
+) => {
+  // const rootStore = getModule(root, store);
+  let response = err.response || {
+    data: {
+      status: 'error',
+      errors: { other: ['レスポンスエラーが発生しました。'] },
+      raw: err
+    }
+  };
+  try {
+    if (!response.data) {
+      response.data = {};
+      response.data.errors = {
+        other: ['サーバーエラーが発生しました。', 'errorCode: 101']
+      };
+    }
+    if (response.data && !Object.keys(response.data.errors).length) {
+      response.data.errors = {
+        other: ['サーバーエラーが発生しました。', response.data.message]
+      };
+    }
+  } catch (error) {
+    response = {
+      data: {
+        status: 'error',
+        errors: { other: ['エラーが発生しました。', error.message] },
+        raw: error
+      }
+    };
+  }
+
+  loader.clear();
+  // rootStore.hideLoader();
+
+  if (!hideErrorMessage) {
+    // rootStore.setErrors(flatSingle(Object.values(response.data.errors)));
+  }
+
+  return Promise.reject(response);
+};
+
+type OptionType = {
+  hideErrorMessage?: boolean;
+  hideLoader?: boolean;
+  baseURL?: string;
+};
+
+export default  (
+  config: AxiosRequestConfig,
+  options: OptionType = {}
+) => {
+  const requestConfig = {
+    ...requestDefaultConfig,
+    ...config,
+    baseURL:
+      options.baseURL == null ? requestDefaultConfig.baseURL : options.baseURL
+  };
+
+  const isDisplayLoader = !options.hideLoader;
+  if (isDisplayLoader) {
+    loader.increment();
+  }
+  return axios(requestConfig)
+    .then((res) => {
+      if (isDisplayLoader) {
+        loader.decrement();
+        if (loader.completed()) {
+
+        }
+      }
+
+      return Promise.resolve(res.data);
+    })
+    .catch(errorHandler(options.hideErrorMessage));
+};
